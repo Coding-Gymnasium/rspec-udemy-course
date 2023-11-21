8b06cf300878def35682421cfef36cb165f326a9
Nico Rithner <nicorithner@gmail.com>
2023-11-20 21:41:04 -0700

    Refactor to use let method

--- rspec-practice/spec/card_spec.rb
+++ rspec-practice/spec/card_spec.rb
@@ -2,7 +2,7 @@
 
 # Card class
 class Card
-  attr_reader :rank, :suit
+  attr_accessor :rank, :suit
 
   def initialize(rank, suit)
     @rank = rank
@@ -12,13 +12,11 @@ end
 
 ## TESTS
 RSpec.describe Card do
+  # Let provides separation between different examples, memoization within an example and lazy loading.
+  let(:card) { Card.new("Ace", "Spades") }
+
+  # With lazy loading. Use a bang after the let. It behaves like a before each block.
   # let!(:card) { Card.new("Ace", "Spades") }
-  # before :each do
-  #   @card = Card.new("Ace", "Spades")
-  # end
-  def card
-    Card.new("Ace", "Spades")
-  end
 
   it "has a suit" do
     expect(card.suit).to eql("Spades")
@@ -26,5 +24,11 @@ RSpec.describe Card do
 
   it "has a rank" do
     expect(card.rank).to eql("Ace")
+  end
+
+  it "can change a rank" do
+    expect(card.rank).to eql("Ace")
+    card.rank = "Queen"
+    expect(card.rank).to eq("Queen")
   end
 end
