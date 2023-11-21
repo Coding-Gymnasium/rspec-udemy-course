05f4b12ad5ccfbcd6f9e6aa3bfae42fae819a96e
Nico Rithner <nicorithner@gmail.com>
2023-11-20 20:27:24 -0700

    Use before each block

--- rspec-practice/spec/card_spec.rb
+++ rspec-practice/spec/card_spec.rb
@@ -2,23 +2,26 @@
 
 # Card class
 class Card
-  attr_reader :rank, :suite
+  attr_reader :rank, :suit
 
-  def initialize(rank, suite)
+  def initialize(rank, suit)
     @rank = rank
-    @suite = suite
+    @suit = suit
   end
 end
 
 ## TESTS
 RSpec.describe Card do
-  let!(:card) { Card.new("Ace", "Spades") }
+  # let!(:card) { Card.new("Ace", "Spades") }
+  before :each do
+    @card = Card.new("Ace", "Spades")
+  end
 
-  it "has a suite" do
-    expect(card.suite).to eql("Spades")
+  it "has a suit" do
+    expect(@card.suit).to eql("Spades")
   end
 
   it "has a rank" do
-    expect(card.rank).to eql("Ace")
+    expect(@card.rank).to eql("Ace")
   end
 end
