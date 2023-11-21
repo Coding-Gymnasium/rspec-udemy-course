f29349b49bb9e559983fa8bbbed229828294292c
Nico Rithner <nicorithner@gmail.com>
2023-11-20 21:22:04 -0700

    Refactor to use a method to instanciate instead

--- rspec-practice/spec/card_spec.rb
+++ rspec-practice/spec/card_spec.rb
@@ -13,15 +13,18 @@ end
 ## TESTS
 RSpec.describe Card do
   # let!(:card) { Card.new("Ace", "Spades") }
-  before :each do
-    @card = Card.new("Ace", "Spades")
+  # before :each do
+  #   @card = Card.new("Ace", "Spades")
+  # end
+  def card
+    Card.new("Ace", "Spades")
   end
 
   it "has a suit" do
-    expect(@card.suit).to eql("Spades")
+    expect(card.suit).to eql("Spades")
   end
 
   it "has a rank" do
-    expect(@card.rank).to eql("Ace")
+    expect(card.rank).to eql("Ace")
   end
 end
