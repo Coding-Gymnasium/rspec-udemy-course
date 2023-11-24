7879a8f495fe3c2a6afbe8390f2369ba89de764b
Nico Rithner <nicorithner@gmail.com>
2023-11-24 14:32:44 -0700

    Explore not_to method

---
+++ rspec-practice/spec/not_to_method_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe "not_to method" do
+  it "checks that two values do not match" do
+    expect(5).not_to eq(2)
+
+    expect("Hello").not_to eq("hello")
+
+    expect([1, 2]).not_to eq([1, 2, 3])
+  end
+end
