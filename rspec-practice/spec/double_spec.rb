ca7d7ce38f37440c7157d43cd8f37ec2f248a6fc
Nico Rithner <nicorithner@gmail.com>
2023-11-30 19:46:58 -0700

    Use allow with receive_messages

--- rspec-practice/spec/double_spec.rb
+++ rspec-practice/spec/double_spec.rb
@@ -7,10 +7,15 @@ RSpec.describe "a random double" do
     # expect(stuntman.fall_off_ladder).to eq("Ouch")
     # expect(stuntman.light_on_fire).to eq(true)
 
-    stuntman = double("Mr. Danger")
-    allow(stuntman).to receive(:fall_off_ladder).and_return("Ouch")
+    # stuntman = double("Mr. Danger")
+    # allow(stuntman).to receive(:fall_off_ladder).and_return("Ouch")
+    #
+    # # expect(stuntman.fall_off_ladder).to be_nil
+    # expect(stuntman.fall_off_ladder).to eq("Ouch")
 
-    # expect(stuntman.fall_off_ladder).to be_nil
+    stuntman = double("Mr. Danger")
+    allow(stuntman).to receive_messages(fall_off_ladder: "Ouch", light_on_fire: true)
     expect(stuntman.fall_off_ladder).to eq("Ouch")
+    expect(stuntman.light_on_fire).to eq(true)
   end
 end
