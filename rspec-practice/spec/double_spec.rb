5d917b9869e548b9b649c1109d9ae8362887e16d
Nico Rithner <nicorithner@gmail.com>
2023-11-30 19:42:17 -0700

    Use double method and with mocked methods

---
+++ rspec-practice/spec/double_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+RSpec.describe "a random double" do
+  it "only allows defined methods to be invoked" do
+    stuntman = double("Mr. Danger", fall_off_ladder: "Ouch", light_on_fire: true)
+
+    expect(stuntman.fall_off_ladder).to eq("Ouch")
+    expect(stuntman.light_on_fire).to eq(true)
+  end
+end
